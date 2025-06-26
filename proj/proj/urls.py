779fe0b5d72cbe2c164c0eb7a343c64a191ebb9c
V.Uthaya kumar <143936637+Uthaya001@users.noreply.github.com>
2025-06-26 22:33:50 +0530

    Update urls.py

--- proj/proj/urls.py
+++ proj/proj/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admi
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from .import views
