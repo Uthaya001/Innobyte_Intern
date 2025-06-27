78a529d32a56f3070ee57721702529b193a8b241
V.Uthaya kumar <143936637+Uthaya001@users.noreply.github.com>
2025-06-27 23:58:38 +0530

    Update views.py

--- proj/proj/views.py
+++ proj/proj/views.py
@@ -2,7 +2,6 @@ from rest_framework import viewsets
 from .serializers import loginSerializer
 from .models import login 
 
-
 class loginview(viewsets.ModelViewSet):
     queryset=login.objects.all()
     serializer_class=loginSerializer
